171633e18f55f81babeee8842221e09ef00ffc02
jiangqiang <479115947@qq.com>
2018-07-12 16:10:10 +0800

    diff

--- lodash/jiangqiangdididi-lodash.js
+++ lodash/jiangqiangdididi-lodash.js
@@ -14,6 +14,27 @@ var jiangqiangdididi={
        }
      }
      return result;
+  },
+  concat:(array,...value)=>{
+      for(var i=0;i<value.length;i++){
+        array.push(value[i]);
+      }
+  },
+  difference:(array,...value)=>{
+    var result=[];
+    var state=0;
+    for(var i=0;i<array.length;i++){
+      for(var s=0;s<value.length;s++){
+        if(value.indexOf(arrat[i])>0){
+          state=1;
+          break;
+        }
+      }
+      if(state===0){
+        result.push(value[i]);
+      }
+      state=0;
+    }
   }
 }
   
