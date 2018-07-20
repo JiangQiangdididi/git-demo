372e66e6e51b663fd4d16daa72980f9d3f35d1b5
jiangqiang <479115947@qq.com>
2018-07-20 23:11:11 +0800

    flattenDepth

--- lodash/jiangqiangdididi-lodash.js
+++ lodash/jiangqiangdididi-lodash.js
@@ -125,7 +125,7 @@ var jiangqiangdididi={
     for(var i=0;i<array.length;i++){
       if(Array.isArray( array[i])){	
         if(depth<0) return buff;
-        buff=buff.concat(this.flattenDeep(array[i]),depth);
+        buff=buff.concat(this.flattenDepth(array[i],depth));
       }else{ console.log(array[i]);
         buff.push(array[i]);
       }
